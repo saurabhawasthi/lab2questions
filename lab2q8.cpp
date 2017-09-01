2665be1f22a60c5594ce6a05d937ea298047314b
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 20:49:59 +0530

    Create lab2q8.cpp

---
+++ lab2q8.cpp
@@ -0,0 +1,17 @@
+# lab2question8
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ float a,b,h;
+
+
+    cout <<"\nenter base of triangle =";
+    cin>>b;
+    cout <<"\nenter height of triangle =";
+    cin>>h;
+    a=(b*h)/2;
+    cout<<"\narea of triangle  ="<<a;
+    return 0;
+}
