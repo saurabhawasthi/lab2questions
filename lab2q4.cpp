9e227774bd7ca3565340946685afada2b9b7c152
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 20:29:03 +0530

    Create lab2q4.cpp

---
+++ lab2q4.cpp
@@ -0,0 +1,16 @@
+# lab2questions4
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int y,d,w;
+cout <<"enter no.of days:";
+cin>>d;
+y=d/365;
+y=d%365;
+w=d/7;
+d=d/7;
+cout <<"\n years: "<<y<<"\n weaks;\:";
+return 0;
+}
