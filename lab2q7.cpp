da29386cba58f8fd5fb88094b1568baa2490ae74
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 20:49:15 +0530

    Create lab2q7.cpp

---
+++ lab2q7.cpp
@@ -0,0 +1,17 @@
+# lab2question7
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ float a1,a2,a3;
+
+
+    cout <<"\nenter 1st angle of triangle =";
+    cin>>a1;
+    cout <<"\nenter 2nd angle of triangle =";
+    cin>>a2;
+    a3=180-(a1+a2);
+    cout<<"\n3rd angle ="<<a3;
+    return 0;
+}
