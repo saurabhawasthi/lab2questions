3af0acfc6b17e6f933f538732b414e05ba4fab37
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 20:01:21 +0530

    Create lab2ques12.cpp

---
+++ lab2ques12.cpp
@@ -0,0 +1,16 @@
+# lab2questions
+# include <iostream>
+using namespace std;
+int main()
+{
+int p,r,t,i;
+cout<<"enter principal : ";
+cin>p;
+cout<<"enter rate : ";
+cin>>r;
+cout<<"enter time : ";
+cin>>t;
+i=(p*r*t)/100;
+cout<<"simple interest is  : "<<i;
+return 0;
+}
