e68d601653b0f677e4da86a8d1f8a1f96206d895
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 19:49:01 +0530

    Create lab 2 que1.cpp

---
+++ lab 2 que1.cpp
@@ -0,0 +1,13 @@
+# lab2questions
+#include <iosterem>
+using namespacestd;
+{
+float l,m,km;
+cout<<"\nenter length in cm =";
+cin>>l;
+m=l/100;
+km=l/100000;
+cout<<"\n length in metre="<<m;
+cout<<"\n length in kilometre="<<km;
+return 0;
+}
