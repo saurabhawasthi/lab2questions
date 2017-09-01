c4270ba5f7031b50bb66b3e2ce15348f851379ca
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 20:47:10 +0530

    Create lab2q3.cpp

---
+++ lab2q3.cpp
@@ -0,0 +1,15 @@
+# lab2question3
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ float c,f;
+
+    cout <<"enter temperature in fahrenheit="<< endl;
+    cin>>f;
+    c=(5*(f-32))/9;
+    cout<<"\ntemperature in celsius="<<c;
+    return 0;
+}
