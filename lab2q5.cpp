646bcad5d1fd29d5e4c71149acce09df8e2745de
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 20:47:51 +0530

    Create lab2q5.cpp

---
+++ lab2q5.cpp
@@ -0,0 +1,17 @@
+# lab2question5
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+ float x,y,v;
+
+    cout <<"\nenter a number =";
+    cin>>x;
+    cout<<"\nenter the power of no.=";
+    cin>>y;
+    v=pow(x,y);
+    cout<<"\nvalue="<<v;
+    return 0;
+}
