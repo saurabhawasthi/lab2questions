863c878486d058cd55e2c5987e5e8642c9049652
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 20:50:41 +0530

    Create lab2q9.cpp

---
+++ lab2q9.cpp
@@ -0,0 +1,14 @@
+# lab2question9
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ float a,area;
+
+    cout <<"\nenter side of triangle =";
+    cin>>a;
+    area=(1.732*a*a)/4;
+    cout<<"\narea of triangle  ="<<area;
+    return 0;
+}
