8b498776c5dfcb3e584b7c83c675dbc74b822397
saurabhawasthi <31364378+saurabhawasthi@users.noreply.github.com>
2017-09-01 20:48:34 +0530

    Create lab2q6.cpp

---
+++ lab2q6.cpp
@@ -0,0 +1,15 @@
+# lab2question6
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+ float n,s;
+
+    cout <<"\nenter a number =";
+    cin>>n;
+    s=sqrt(n);
+    cout<<"\nsquare root of given  no.="<<s;
+    return 0;
+}
